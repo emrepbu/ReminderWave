b32f36c2d3e18a2b5170dc23dc598881ae6b6490
emrepbu <33737009+emrepbu@users.noreply.github.com>
2025-04-16 22:41:01 +0300

    Create notification service

--- ReminderWave/Services/NotificationService.swift
+++ ReminderWave/Services/NotificationService.swift
@@ -6,3 +6,61 @@
 //
 
 import Foundation
+import UserNotifications
+
+protocol NotificationServiceProtocol {
+    func requestAuthorization(completion: @escaping (Bool) -> Void)
+    func scheduleNotification(for task: Task)
+    func cancelNotification(for taskId: UUID)
+}
+
+class LocalNotificationService: NotificationServiceProtocol {
+    
+    func requestAuthorization(completion: @escaping (Bool) -> Void) {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+            DispatchQueue.main.async {
+                completion(success)
+            }
+        }
+    }
+    
+    func scheduleNotification(for task: Task) {
+        let content = UNMutableNotificationContent()
+        content.title = String(localized: "Task Reminder")
+        content.body = task.title
+        content.sound = UNNotificationSound.default
+        
+        let triggerDate = Calendar.current.dateComponents([
+            .year,
+            .month,
+            .day,
+            .hour,
+            .minute
+        ], from: task.dueDate)
+        
+        let trigger = UNCalendarNotificationTrigger(
+            dateMatching: triggerDate,
+            repeats: false
+        )
+        
+        let request = UNNotificationRequest(
+            identifier: task.id.uuidString,
+            content: content,
+            trigger: trigger
+        )
+        
+        UNUserNotificationCenter.current().add(request) { error in
+            
+            if let error = error {
+                print("Error scheduling notification: \(error)")
+            }
+        }
+    }
+    
+    func cancelNotification(for taskId: UUID) {
+        UNUserNotificationCenter.current()
+            .removePendingNotificationRequests(
+                withIdentifiers: [taskId.uuidString]
+            )
+    }
+}
